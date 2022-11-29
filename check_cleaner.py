43ec9e4402a2ed61d702505a524992311fa5a872
Yehor S <egorsmkv@gmail.com>
2022-11-29 19:06:34 +0000

    Add a new useful script

---
+++ check_cleaner.py
@@ -0,0 +1,17 @@
+import json
+from text import cleaners
+
+ROWS_FILE = '/home/yehor/ML/flowtron/dataset_lada_trimmed/accept/metadata.jsonl'
+
+with open(ROWS_FILE) as x:
+    dataset = [json.loads(s) for s in x.readlines()]
+
+print(dataset[:3])
+
+for s in dataset:
+    text = s['orig_text']
+    text = cleaners.flowtron_cleaners(text)
+
+    print(s['orig_text'])
+    print(text)
+    print()
